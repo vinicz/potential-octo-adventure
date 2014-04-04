30cabded894ed25c48e71bc3f14fd1727cd0f8d9
vinicz <bayusxz@hotmail.com>
2014-04-05 01:28:37 +0200

    Implemented accelomeer camera tilt

--- Assets/Scripts/CameraAccelometerController.cs
+++ Assets/Scripts/CameraAccelometerController.cs
@@ -1,17 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraAccelometerController : MonoBehaviour {
+public class CameraAccelometerController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public Vector3 initialAcceleration = new Vector3(0, 0, -1);
+    public float maxRotationAngle;
+    private Vector3 initialCameraForward;
+    private Quaternion initialCameraOrientation;
 
-        Vector3 lookAt = this.transform.LookAt();
-	
-	}
+    void Start()
+    {
+        initialCameraForward = this.transform.forward;
+        initialCameraOrientation = this.transform.rotation;
+    }
+
+    void Update()
+    {
+
+        Vector3 fixedAcceleration = new Vector3(
+                Input.acceleration.x, 
+                Input.acceleration.z, 
+                Input.acceleration.y);
+
+
+        Quaternion fullRotation = Quaternion.FromToRotation(initialCameraForward, fixedAcceleration);
+        float xRotation = fullRotation.eulerAngles.x;
+        float zRotation = fullRotation.eulerAngles.z;
+
+        if (xRotation > maxRotationAngle && xRotation < 180)
+        {
+            xRotation = maxRotationAngle;
+        }
+        if (xRotation < 360 - maxRotationAngle && xRotation > 180)
+        {
+            xRotation = 360 - maxRotationAngle;
+        }
+        if (zRotation > maxRotationAngle && zRotation < 180)
+        {
+            zRotation = maxRotationAngle;
+        }
+        if (zRotation < 360 - maxRotationAngle && zRotation > 180)
+        {
+            zRotation = 360 - maxRotationAngle;
+        }
+        Quaternion fixedYRotation = Quaternion.Euler(xRotation, 0, zRotation);
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, fixedYRotation * initialCameraOrientation, Time.deltaTime);
+       
+    }
 }
---
+++ Assets/Scripts/RotationLocker.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationLocker : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.eulerAngles=new Vector3(transform.eulerAngles.x,0,transform.eulerAngles.z);
+	}
+}
