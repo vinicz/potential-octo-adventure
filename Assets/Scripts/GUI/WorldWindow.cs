8e60d87b2d331d53ef2acd1418a6f17d0df29646
vinicz <bayusxz@hotmail.com>
2014-04-22 22:44:58 +0200

    -added reward counter -adjusted GUI layout

--- Assets/Scripts/GUI/WorldWindow.cs
+++ Assets/Scripts/GUI/WorldWindow.cs
@@ -9,25 +9,44 @@ public class WorldWindow : MonoBehaviour
     public string wordName;
     public WindowOpenerScript backButton;
     public UILabel windowTitle;
-    public UILabel collectedDiamondsLabel;
 
     // Use this for initialization
     void Start()
     {
         int collectedDiamonds = 0;
         int allDiamonds = 0;
+        int levelCounter = 15;
+        GameObject currentParentElement = null;
+        int rowCounter = 0;
+        int columnCounter = 0;
 
         foreach (LevelRecord level in GameServiceLayer.serviceLayer.levelService.getLevelsInWorld(wordName))
         {
-            levelItem.createLevelItem(parentGrid.gameObject, level, this.gameObject);
-            collectedDiamonds += level.collectedRewards;
-            allDiamonds += 3;
+            if(columnCounter==5)
+            {
+                columnCounter = 0;
+                rowCounter++;
+            }
+
+            if(levelCounter==15)
+            {
+                currentParentElement = NGUITools.AddChild(parentGrid.gameObject, new GameObject());
+                levelCounter = 0;
+                rowCounter = 0;
+            }
+
+            GameObject newLevelItem = levelItem.createLevelItem(currentParentElement, level, this.gameObject);
+            newLevelItem.transform.position = new Vector3(newLevelItem.transform.position.x+columnCounter*0.25f,
+                                                          newLevelItem.transform.position.y,
+                                                          newLevelItem.transform.position.z-rowCounter*0.15f+0.25f);
+
+            levelCounter++;
+            columnCounter++;
         }
 
         parentGrid.Reposition();
 
         windowTitle.text = wordName;
-        collectedDiamondsLabel.text = collectedDiamonds + "/" + allDiamonds;
 
     }
 
