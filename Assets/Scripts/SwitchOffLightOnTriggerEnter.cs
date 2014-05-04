2afdae10cba13419717158eb6d843be1f1552fa0
mkghoster <mkghoster@gmail.com>
2014-05-04 20:05:34 +0200

    some bouncy levels

---
+++ Assets/Scripts/SwitchOffLightOnTriggerEnter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchOffLightOnTriggerEnter : MonoBehaviour {
+
+	public GameObject light;
+
+	void OnTriggerEnter(Collider other) {
+		
+		if (other.gameObject.tag == "Player") {
+			light.SetActive(false);			
+		}
+	}
+
+}
