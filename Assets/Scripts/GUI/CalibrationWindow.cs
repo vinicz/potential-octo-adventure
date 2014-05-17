74e78140424425abd9386fb6f8c48a93ff1d1d85
vinicz <bayusxz@hotmail.com>
2014-05-17 20:50:54 +0200

    -fixed initial orientation

---
+++ Assets/Scripts/GUI/CalibrationWindow.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CalibrationWindow : MonoBehaviour
+{
+
+    public UILabel calibrationLabel;
+    public GameObject previousWindow;
+    private bool beforeCalibration = true;
+    private bool calibrationFinished = false;
+    private List<GameObject> activeWindows = new List<GameObject>();
+
+    void Start()
+    {
+        disableOtherWindows();
+
+    }
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            if (beforeCalibration)
+            {
+                calibrationLabel.text = "Calibrating..";
+                beforeCalibration = false;
+
+                GameServiceLayer.serviceLayer.optionsService.CalibrationCompleted += onCalibrationCompleted;
+                GameServiceLayer.serviceLayer.optionsService.calibrateInitialOrientation();
+            }
+
+            if (calibrationFinished)
+            {
+                calibrationFinished = false;
+                this.gameObject.SetActive(false);
+                previousWindow.SetActive(true);
+
+                foreach (var window in activeWindows)
+                {
+                    window.SetActive(true);
+                }
+            }
+
+        }
+    }
+
+    void onCalibrationCompleted()
+    {
+        calibrationFinished = true;
+        calibrationLabel.text = "Calibration finished,\n tap to exit";
+      
+    }
+
+    void disableOtherWindows()
+    {
+        foreach (Transform window in this.transform.parent)
+        {
+            if (window != this.transform)
+            {
+                activeWindows.Add(window.gameObject);
+                window.gameObject.SetActive(false);
+            }
+        }
+    }
+}
