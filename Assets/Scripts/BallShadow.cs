12af6afff4a24006142907da32ccb721a98b927c
mkghoster <mkghoster@gmail.com>
2014-05-18 19:20:26 +0200

    Added projector shadow to the ball

---
+++ Assets/Scripts/BallShadow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallShadow : MonoBehaviour {
+
+	private Quaternion fixedRotation;
+
+	void Start() {
+		fixedRotation = transform.rotation;
+		//fixedRotation = Quaternion.LookRotation (Vector3.down, Vector3.forward);
+	}
+
+	void LateUpdate() {
+		transform.rotation = fixedRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.rotation = fixedRotation;	
+	}
+}
