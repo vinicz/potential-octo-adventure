32e21550e6f0dbd0011d7c92a68cf774021469d9
mkghoster <mkghoster@gmail.com>
2014-05-20 18:02:09 +0200

    Added some fire levels + SmokeStorm prefab

---
+++ Assets/Scripts/SmokeStormIncrease.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmokeStormIncrease : MonoBehaviour {
+
+	public float targetEmission = 100;
+	public float time = 100;
+
+	private ParticleSystem particleSystem;
+	private float step;
+
+	void Start () {
+		particleSystem = GetComponent<ParticleSystem> ();
+		step = (targetEmission - particleSystem.emissionRate) / time;
+	}
+
+	void Update () {
+		if(GameServiceLayer.serviceLayer.gameMaster.getGameState() == GameHandlerScript.GameState.GAME)
+			particleSystem.emissionRate += step * Time.deltaTime;
+	}
+}
