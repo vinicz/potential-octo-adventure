0877316d3256ff8299a82b36e927ca6a9c5328dd
vinicz <bayusxz@hotmail.com>
2014-05-30 14:50:47 +0200

    -added leveldata collector

--- Assets/Scripts/GameServiceLayer/GameServiceLayer.cs
+++ Assets/Scripts/GameServiceLayer/GameServiceLayer.cs
@@ -2,26 +2,38 @@ using UnityEngine;
 using System.Collections;
 
 public class GameServiceLayer : MonoBehaviour {
-
-
-    public static GameServiceLayer serviceLayer;
-
-    public GameHandlerScript gameMaster;
-    public LevelService levelService;
-    public ItemService itemService;
-    public OptionsService optionsService;
-    public BackgroundMusicPlayer backgroundMusicPlayer;
-
-	void Awake () {
-
-        if (serviceLayer == null)
-        {
-            DontDestroyOnLoad(gameObject);
-            serviceLayer = this;
-        } else if (serviceLayer != this)
-        {
-            Destroy(gameObject);
-        }
 	
+	
+	public static GameServiceLayer serviceLayer;
+	
+	public GameHandlerScript gameMaster;
+	public LevelService levelService;
+	public ItemService itemService;
+	public OptionsService optionsService;
+	public BackgroundMusicPlayer backgroundMusicPlayer;
+	
+	public delegate void LevelConfigurationFinishedHandler();
+	public event LevelConfigurationFinishedHandler LevelConfigurationFinished;
+	
+	void Awake () {
+		
+		if (serviceLayer == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			serviceLayer = this;
+		} else if (serviceLayer != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+	public void setCurrentGameMaster(GameHandlerScript master)
+	{
+		this.gameMaster = master;
+		
+		if(LevelConfigurationFinished != null)
+		{
+			LevelConfigurationFinished();
+		}
 	}
 }
