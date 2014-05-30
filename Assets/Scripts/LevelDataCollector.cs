5578c6e00fc7124389bc19e795bee9c1fe356e5f
vinicz <bayusxz@hotmail.com>
2014-05-30 23:25:49 +0200

    -added data collection support for diamond collection times

--- Assets/Scripts/LevelDataCollector.cs
+++ Assets/Scripts/LevelDataCollector.cs
@@ -5,127 +5,122 @@ using System.Collections.Generic;
 public class LevelDataCollector : MonoBehaviour
 {
 	
-	public static  LevelDataCollector levelDebugMonitor;
-	private List<float> diamondCollectionTimes = new List<float>();
-	private float gameTime;
-	private LevelRecord currentLevel;
-	private bool uploadStarted;
-	private WWW ramballServerUpload;
+		public static  LevelDataCollector levelDebugMonitor;
+		private List<float> diamondCollectionTimes = new List<float> ();
+		private float gameTime;
+		private LevelRecord currentLevel;
+		private bool uploadStarted;
+		private WWW ramballServerUpload;
 	
-	void Start()
-	{
-		
-		if (levelDebugMonitor == null)
+		void Start ()
 		{
-			levelDebugMonitor = this;
-			levelDebugMonitor.initLevelDataCollector();
-			DontDestroyOnLoad(this);
+		
+				if (levelDebugMonitor == null) {
+						levelDebugMonitor = this;
+						levelDebugMonitor.initLevelDataCollector ();
+						DontDestroyOnLoad (this);
 			
-			GameServiceLayer.serviceLayer.LevelConfigurationFinished += initLevelDataCollector;
+						GameServiceLayer.serviceLayer.LevelConfigurationFinished += initLevelDataCollector;
 			
-		} else if (levelDebugMonitor != this)
-		{
-			Destroy(this);
-		}
-		
-	}
-	
-	public void initLevelDataCollector()
-	{
-		
-		if (GameServiceLayer.serviceLayer.gameMaster != null)
-		{
-			GameServiceLayer.serviceLayer.gameMaster.CollectedDiamondCountChanged += onCollectedDiamondCountChanged;
-			GameServiceLayer.serviceLayer.gameMaster.LevelFailed += onLevelFailed;
-			GameServiceLayer.serviceLayer.gameMaster.LevelPassed += onLevelPassed;
-			gameTime = GameServiceLayer.serviceLayer.gameMaster.getElapsedTime();
-			currentLevel = GameServiceLayer.serviceLayer.gameMaster.getCurrentLevelRecord();
-			diamondCollectionTimes.Clear();
-			uploadStarted = false;
-		}
-		
-		
-	}
-	
-	void Update()
-	{
-		if (GameServiceLayer.serviceLayer.gameMaster != null)
-		{
-			gameTime = GameServiceLayer.serviceLayer.gameMaster.getElapsedTime();
-		}
-	}
-	
-	void OnGUI()
-	{
-		if (currentLevel != null)
-		{
-			GUI.Box(new Rect(0, 0, 150, (diamondCollectionTimes.Count + 3) * 30), currentLevel.levelName);
-			GUI.Label(new Rect(0, 30, 150, 30), "Time: " + gameTime.ToString());
-			
-			int diamondCounter = 0;
-			foreach (float diamondTime in diamondCollectionTimes)
-			{
-				GUI.Label(new Rect(0, 60 + diamondCounter * 30, 150, 300), "Diamond" + diamondCounter + ": " + diamondTime);
-				
-				diamondCounter++;
-			}
-			if (uploadStarted)
-			{
-				
-				if (ramballServerUpload.error != null)
-				{
-					GUI.Label(new Rect(0, (diamondCollectionTimes.Count + 2) * 30, 150, 300), "Couldnt upload result!");
-				} else if (ramballServerUpload.isDone && ramballServerUpload.text.Equals("OK"))
-				{
-					GUI.Label(new Rect(0, (diamondCollectionTimes.Count + 2) * 30, 150, 300), "Result uploaded!");
-				}  else
-				{
-					GUI.Label(new Rect(0, (diamondCollectionTimes.Count + 2) * 30, 150, 300), "Uploading result...");
+				} else if (levelDebugMonitor != this) {
+						Destroy (this);
 				}
-				
-				
-			}
+		
 		}
-	}
 	
-	void onCollectedDiamondCountChanged()
-	{
-		diamondCollectionTimes.Add(GameServiceLayer.serviceLayer.gameMaster.getElapsedTime());
+		public void initLevelDataCollector ()
+		{
 		
-	}
-	
-	void onLevelPassed()
-	{
-		onLevelEnded(true);
-	}
-	
-	void onLevelFailed()
-	{
-		onLevelEnded(false);
-	}
-	
-	void onLevelEnded(bool levelPassed)
-	{
-		string requestURL = "http://ramballserver-mudstudio.rhcloud.com/rest/LevelData/submit?";
-		requestURL += "levelName=" + currentLevel.levelName;
-		requestURL += "&levelPassed=" + levelPassed.ToString();
-		requestURL += "&gameTime=" + gameTime.ToString();
+				if (GameServiceLayer.serviceLayer.gameMaster != null) {
+						GameServiceLayer.serviceLayer.gameMaster.CollectedDiamondCountChanged += onCollectedDiamondCountChanged;
+						GameServiceLayer.serviceLayer.gameMaster.LevelFailed += onLevelFailed;
+						GameServiceLayer.serviceLayer.gameMaster.LevelPassed += onLevelPassed;
+						gameTime = GameServiceLayer.serviceLayer.gameMaster.getElapsedTime ();
+						currentLevel = GameServiceLayer.serviceLayer.gameMaster.getCurrentLevelRecord ();
+						diamondCollectionTimes.Clear ();
+						uploadStarted = false;
+				}
 		
-		ramballServerUpload = new WWW(requestURL);
-		uploadStarted = true; 
-	}
+		
+		}
 	
-	void OnDestroy()
-	{
-		GameServiceLayer.serviceLayer.LevelConfigurationFinished -= initLevelDataCollector;
-	}
+		void Update ()
+		{
+				if (GameServiceLayer.serviceLayer.gameMaster != null) {
+						gameTime = GameServiceLayer.serviceLayer.gameMaster.getElapsedTime ();
+				}
+		}
 	
+		void OnGUI ()
+		{
+				if (currentLevel != null) {
+						GUI.Box (new Rect (0, 0, 150, (diamondCollectionTimes.Count + 3) * 30), currentLevel.levelName);
+						GUI.Label (new Rect (0, 30, 150, 30), "Time: " + gameTime.ToString ());
+			
+						int diamondCounter = 0;
+						foreach (float diamondTime in diamondCollectionTimes) {
+								GUI.Label (new Rect (0, 60 + diamondCounter * 30, 150, 300), "Diamond" + diamondCounter + ": " + diamondTime);
+				
+								diamondCounter++;
+						}
+						if (uploadStarted) {
+				
+								if (ramballServerUpload.error != null) {
+										GUI.Label (new Rect (0, (diamondCollectionTimes.Count + 2) * 30, 150, 300), "Couldnt upload result!");
+								} else if (ramballServerUpload.isDone && ramballServerUpload.text.Equals ("OK")) {
+										GUI.Label (new Rect (0, (diamondCollectionTimes.Count + 2) * 30, 150, 300), "Result uploaded!");
+								} else {
+										GUI.Label (new Rect (0, (diamondCollectionTimes.Count + 2) * 30, 150, 300), "Uploading result...");
+								}
+				
+				
+						}
+				}
+		}
 	
+		void onCollectedDiamondCountChanged ()
+		{
+				diamondCollectionTimes.Add (GameServiceLayer.serviceLayer.gameMaster.getElapsedTime ());
+		
+		}
 	
+		void onLevelPassed ()
+		{
+				onLevelEnded (true);
+		}
 	
+		void onLevelFailed ()
+		{
+				onLevelEnded (false);
+		}
 	
+		void onLevelEnded (bool levelPassed)
+		{
+				string requestURL = "http://ramball-mudstudio.rhcloud.com/rest/LevelData/submit?";
+				requestURL += "levelName=" + currentLevel.levelName;
+				requestURL += "&levelPassed=" + levelPassed.ToString ();
+				requestURL += "&gameTime=" + gameTime.ToString ();
+				requestURL += "&diamondCollections=";
+
+				int counter = 1;
+				foreach (float diamondCollectionTime in diamondCollectionTimes) {
+						requestURL += diamondCollectionTime.ToString();
+
+						if (counter != diamondCollectionTimes.Count) {
+								requestURL += ",";
+						}
+
+						counter++;
+			
+				}
+
+				ramballServerUpload = new WWW (requestURL);
+				uploadStarted = true; 
+		}
 	
-	
-	
-	
+		void OnDestroy ()
+		{
+				GameServiceLayer.serviceLayer.LevelConfigurationFinished -= initLevelDataCollector;
+		}
+
 }
