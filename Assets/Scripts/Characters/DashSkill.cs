d4af55b9c70ce1b5216e5b49fff372510f7213a3
vinicz <bayusxz@hotmail.com>
2014-06-07 15:31:31 +0200

    -added dashing character

---
+++ Assets/Scripts/Characters/DashSkill.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DashSkill : Skill {
+
+	public float coolDown;
+	public float force = 1;
+	
+	public static string DASH_SKILL_NAME = "dash_skill";
+	
+	private float cooldownTimer = 0;
+	
+	
+	void Update () {
+		if(cooldownTimer>0)
+		{
+			cooldownTimer -= Time.deltaTime;
+		}
+		
+	}
+	
+	public override void useSkill()
+	{
+		if(cooldownTimer<=0)
+		{
+			Vector3 dashDirection = rigidbody.velocity.normalized;
+
+			rigidbody.AddForce(dashDirection*force);
+			cooldownTimer = coolDown;
+		}
+	}
+	
+	
+	public override float getCoolDownRemaining()
+	{
+		return cooldownTimer;
+	}
+	
+	public override string getSkillName()
+	{
+		return DASH_SKILL_NAME;
+	}
+
+}
