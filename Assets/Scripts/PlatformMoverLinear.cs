37136ce731967c6e4047dbed16fa57a720aa23b8
mkghoster <mkghoster@gmail.com>
2014-06-08 21:23:28 +0200

    Adjusted levels to the bigger box size. Added spike level. IceFloor1Cube prefab.

--- Assets/Scripts/PlatformMoverLinear.cs
+++ Assets/Scripts/PlatformMoverLinear.cs
@@ -9,19 +9,15 @@ public class PlatformMoverLinear : MonoBehaviour {
     private int targetIndex=0;
 	public bool loop = true;
 
-	// Use this for initialization
 	void Start () {
         if(targetList.Count==0)
         {
             enabled=false;
         }
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		if (GameServiceLayer.serviceLayer.gameMaster.getGameState () == GameHandlerScript.GameState.GAME) {
-			//transform.position = Vector3.Lerp (transform.position, targetList [targetIndex].transform.position, Time.deltaTime * speed);
-			//transform.position = Vector3.Lerp (transform.position, targetList [targetIndex].transform.position, Time.deltaTime * speed);
 			transform.position += (targetList [targetIndex].transform.position - transform.position).normalized * Time.deltaTime * speed;
 
 			if ((transform.position - targetList [targetIndex].transform.position).magnitude < 0.2) {
