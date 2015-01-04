3c5e8aa5bff95cd312305d7d32060ebaf818c0f8
mkghoster <mkghoster@gmail.com>
2015-01-04 20:04:42 +0100

    Cleaned normal lv 1-5

---
+++ Assets/Scripts/MapBase.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapBase : MonoBehaviour
+{
+	// In case we fuck up map base position
+	// would be nicer on (0, 0, 0)
+	public Vector3 mapBase = new Vector3 (-2.4838f, -15.044f, 4.5385f);
+}
